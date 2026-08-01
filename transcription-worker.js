0022363af6e7eaec35977979b4673ca31c53af2f
ubaldoorru93-ux <ubaldoorru93@gmail.com>
2026-08-01 21:54:13 +1000

    Use Whisper base.en in transcription test

--- transcription-worker.js
+++ transcription-worker.js
@@ -6,7 +6,7 @@ import {
 env.allowLocalModels = false;
 env.useBrowserCache = true;
 
-const MODEL_ID = "onnx-community/whisper-tiny.en";
+const MODEL_ID = "onnx-community/whisper-base.en";
 let activeDevice = self.navigator?.gpu ? "webgpu" : "wasm";
 
 function modelOptions(device) {
